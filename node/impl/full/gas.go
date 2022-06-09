b2f8ae58b2e562f86e83c3b8419f894d06df5602
simlecode <hj1557986@gmail.com>
2022-06-09 14:06:18 +0800

    fix: fix compile error

--- node/impl/full/gas.go
+++ node/impl/full/gas.go
@@ -338,7 +338,7 @@ func evalMessage(ctx context.Context, smgr *stmgr.StateManager, cstore *store.Ch
 		if err != stmgr.ErrExpensiveFork {
 			break
 		}
-		ts, err = cstore.GetTipSetFromKey(ts.Parents())
+		ts, err = cstore.GetTipSetFromKey(ctx, ts.Parents())
 		if err != nil {
 			return -1, xerrors.Errorf("getting parent tipset: %w", err)
 		}
@@ -412,7 +412,7 @@ func (m *GasModule) GasBatchEstimateMessageGas(ctx context.Context, estimateMess
 		return nil, nil
 	}
 
-	ts, err := m.Chain.GetTipSetFromKey(tsk)
+	ts, err := m.Chain.GetTipSetFromKey(ctx, tsk)
 	if err != nil {
 		return nil, xerrors.Errorf("getting tipset: %w", err)
 	}
