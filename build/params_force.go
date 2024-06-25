d2825a916caf2d25fe4a228ffcf74740ddd1260c
fsgerse <173036932+fsgerse@users.noreply.github.com>
2024-06-25 14:56:51 +0800

    fix: update force params

--- build/params_force.go
+++ build/params_force.go
@@ -4,7 +4,6 @@
 package build
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 
@@ -24,7 +23,7 @@ var NetworkBundle = "testing"
 var BundleOverrides map[actorstypes.Version]string
 var ActorDebugging = true
 
-const GenesisNetworkVersion = network.Version21
+const GenesisNetworkVersion = network.Version22
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 
@@ -68,9 +67,11 @@ var UpgradeThunderHeight = abi.ChainEpoch(-23)
 
 var UpgradeWatermelonHeight = abi.ChainEpoch(-24)
 
-var UpgradeDragonHeight = abi.ChainEpoch(20)
+var UpgradeDragonHeight = abi.ChainEpoch(-25)
 
-var UpgradePhoenixHeight = UpgradeDragonHeight + 120
+var UpgradePhoenixHeight = abi.ChainEpoch(-26)
+
+var UpgradeAussieHeight = abi.ChainEpoch(200)
 
 // This fix upgrade only ran on calibrationnet
 var UpgradeWatermelonFixHeight = abi.ChainEpoch(-100)
@@ -139,6 +140,13 @@ func init() {
 	UpgradeLightningHeight = getUpgradeHeight("LOTUS_LIGHTNING_HEIGHT", UpgradeLightningHeight)
 	UpgradeThunderHeight = getUpgradeHeight("LOTUS_THUNDER_HEIGHT", UpgradeThunderHeight)
 	UpgradeDragonHeight = getUpgradeHeight("LOTUS_DRAGON_HEIGHT", UpgradeDragonHeight)
+	UpgradeAussieHeight = getUpgradeHeight("LOTUS_AUSSIE_HEIGHT", UpgradeAussieHeight)
+
+	UpgradePhoenixHeight = getUpgradeHeight("LOTUS_PHOENIX_HEIGHT", UpgradePhoenixHeight)
+	DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+		0:                    DrandMainnet,
+		UpgradePhoenixHeight: DrandQuicknet,
+	}
 
 	BuildType |= BuildForce
 
@@ -146,8 +154,6 @@ func init() {
 	if newBlockDelaySecs < BlockDelaySecs {
 		BlockDelaySecs = newBlockDelaySecs
 	}
-
-	fmt.Println("BlockDelaySecs:", BlockDelaySecs)
 }
 
 var BlockDelaySecs = uint64(30)
